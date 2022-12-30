018045017166e832ae75cfdd64b1e99e871998e9
Frédéric LURET <frederic.luret@univ-tours.fr>
2022-12-30 21:35:14 +0100

    Suppression des espaces

--- Example01/Example_01.py
+++ Example01/Example_01.py
@@ -16,9 +16,9 @@ for job_element in job_elements:
     title_element = job_element.find("h2", class_="title")
     company_element = job_element.find("h3", class_="company")
     location_element = job_element.find("p", class_="location")
-    print(title_element.text)
-    print(company_element.text)
-    print(location_element.text)
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
     print()
 
 
