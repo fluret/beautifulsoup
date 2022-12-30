9320c418f0cd0aefe47f135e489db14522052cba
Frédéric LURET <frederic.luret@univ-tours.fr>
2022-12-30 21:20:53 +0100

    Recherche de sous éléments

--- Example01/Example_01.py
+++ Example01/Example_01.py
@@ -13,5 +13,11 @@ print(results.prettify())
 job_elements = results.find_all("div", class_="card-content")
 
 for job_element in job_elements:
-    print(job_element, end="\n"*2)
-    
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element)
+    print(company_element)
+    print(location_element)
+    print()
+
