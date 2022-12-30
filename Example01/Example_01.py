944114048ef3fd02b75b1199c00c46e84a7df6b6
Frédéric LURET <frederic.luret@univ-tours.fr>
2022-12-30 21:27:09 +0100

    Récupération du texte uniquement

--- Example01/Example_01.py
+++ Example01/Example_01.py
@@ -16,8 +16,9 @@ for job_element in job_elements:
     title_element = job_element.find("h2", class_="title")
     company_element = job_element.find("h3", class_="company")
     location_element = job_element.find("p", class_="location")
-    print(title_element)
-    print(company_element)
-    print(location_element)
+    print(title_element.text)
+    print(company_element.text)
+    print(location_element.text)
     print()
+
 
