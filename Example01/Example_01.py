b4046c7745e75dfed04485e142060c84712372e0
Frédéric LURET <47600423+fluret@users.noreply.github.com>
2023-01-17 13:15:23 +0100

    Explication bis

--- Example01/Example_01.py
+++ Example01/Example_01.py
@@ -21,14 +21,18 @@ récupère sous la forme d'une liste toutes les divs possédant la classe card-c
 '''
 for job_element in job_elements:
     title_element = job_element.find("h2", class_="title")
-    #
+    #Récupère la première balise h2 avec la classe title ici une seule
     company_element = job_element.find("h3", class_="company")
-    
+    #Récupère la première balise h3 avec la classe company ici une seule
     location_element = job_element.find("p", class_="location")
-    
+    # récupère la première balise p avec la classe location ici une seule
     print(title_element.text.strip())
+    #imprime uniquement le texte de title_element et donc supprime les balises
+    #la fonction strip supprime les esapces en début et en fin de chaine
     print(company_element.text.strip())
+    
     print(location_element.text.strip())
+    
     print()
 
 python_jobs = results.find_all("h2", string=lambda text: "python" in text.lower())
